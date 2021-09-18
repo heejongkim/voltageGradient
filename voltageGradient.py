a4699dad821df03b949c73473410ece58a8e56d2
heejongkim <heejongkim@users.noreply.github.com>
2021-09-18 19:54:26 -0400

    no voltage case exception handling

--- voltageGradient.py
+++ voltageGradient.py
@@ -1,8 +1,8 @@
-## last updated: 2021.06.23
+## last updated: 2021.09.18 - 1
 import warnings
 import re
-import win32com.client as win32
-import serial
+import win32com.client as win32  # pip install pywin32
+import serial                    # pip install pyserial=3.5
 import serial.tools.list_ports
 import datetime
 from datetime import timedelta
@@ -12,6 +12,7 @@ import pandas as pd
 import csv
 import os
 from apscheduler.schedulers.background import BackgroundScheduler
+# pip install apscheduler=3.7.0
 
 def getPowerSupplyOutputLine(serial_info):
     with serial.Serial(serial_info['port'], serial_info['baudRate']) as ser:
@@ -19,11 +20,11 @@ def getPowerSupplyOutputLine(serial_info):
         return('\t'.join([str(datetime.datetime.now()), ser.readline().decode('ascii')]))
 
 def setPowerSupplyVoltage(serial_info, voltage):
-     with serial.Serial(serial_info['port'], serial_info['baudRate']) as ser:
+    with serial.Serial(serial_info['port'], serial_info['baudRate']) as ser:
         ser.write(("V="+str(voltage)).encode('ascii'))
     
 def setPowerSupplyCurrent(serial_info, current):
-     with serial.Serial(serial_info['port'], serial_info['baudRate']) as ser:
+    with serial.Serial(serial_info['port'], serial_info['baudRate']) as ser:
         ser.write(("I="+str(current)).encode('ascii'))
     
 def setPowerSupplyPolarity(serial_info, polarity):
@@ -44,11 +45,11 @@ def scheduleStepVoltage(serial_info, scheduler, volt_gradient_table, gradient_lo
         voltage = round(float(row["voltage(V)"]),2)
         polarity = int(row["polarity"])
         # schedule the current, voltage, and polarity with RT point
-        scheduler.add_job(setPowerSupplyCurrent, 'date', run_date = now + timedelta(minutes= float(row["time(min)"])), args = [serial_info, current])
-        scheduler.add_job(setPowerSupplyVoltage, 'date', run_date = now + timedelta(minutes= float(row["time(min)"]), seconds=1.0), args = [serial_info, voltage])
-        scheduler.add_job(setPowerSupplyPolarity, 'date', run_date = now + timedelta(minutes= float(row["time(min)"]), seconds=2.0), args = [serial_info, polarity])
+        scheduler.add_job(setPowerSupplyVoltage, 'date', run_date = now + timedelta(minutes= float(row["time(min)"]), seconds=3.0), args = [serial_info, voltage])
+        scheduler.add_job(setPowerSupplyCurrent, 'date', run_date = now + timedelta(minutes= float(row["time(min)"]), seconds=6.0), args = [serial_info, current])
+        scheduler.add_job(setPowerSupplyPolarity, 'date', run_date = now + timedelta(minutes= float(row["time(min)"]), seconds=9.0), args = [serial_info, polarity])
         # schedule the log write to prevent blocking the port
-        for log_time in range(4, 16, 2):
+        for log_time in range(12, 24, 2):
             scheduler.add_job(writeGradientLog, 'date', run_date = now + timedelta(minutes= float(row["time(min)"]), seconds=log_time), args = [serial_info, gradient_log_filename])
 
 
@@ -81,7 +82,11 @@ def arduinoPortDetect():
 def extractVoltageSchemeFilename(filename):
     split_raw_filename_by_sp_chars = re.split(r'[`\-=~!@#$%^&*()_+\[\]{};\'\\:"|<,./<>?]', filename)  # https://stackoverflow.com/questions/21023901/how-to-split-at-all-special-characters-with-re-split
     volt_string = [s for s in split_raw_filename_by_sp_chars if "volt" in s]  # https://stackoverflow.com/questions/4843158/check-if-a-python-list-item-contains-a-string-inside-another-string
-    return(volt_string[0] + ".csv")
+    print(volt_string)
+    if volt_string:
+        return(volt_string[0] + ".csv")
+    else:
+        return(volt_string) # return empty list
 
 ### INITIALIZATION
 # PowerSupply Communication Info
@@ -107,23 +112,26 @@ while True:
         print("Acquisition Starts ...")
         # retrieve raw filename from xcalibur
         raw_filename = q.GetMangledRawFilename
-        # set log filename
-        gradient_log_filename = raw_filename[:-4] + ".csv"
         # retrieve voltage scheme
         volt_gradient_filename = extractVoltageSchemeFilename(filename = raw_filename)
         ### probably need to put non voltage gradient exception handling here
-        # read voltage scheme into table - assuming the scheme file is located under the same location where this script is located
-        volt_gradient_table = readGradientTable(volt_gradient_filename)
-        print(volt_gradient_table)
-        # schedule the gradient - nonblocking background scheduling
-        scheduleStepVoltage(serial_info, scheduler, volt_gradient_table, gradient_log_filename)
-        # print schedule
-        scheduler.print_jobs()
+        if volt_gradient_filename:
+            # set log filename
+            gradient_log_filename = raw_filename[:-4] + ".csv"
+            # read voltage scheme into table - assuming the scheme file is located under the same location where this script is located
+            volt_gradient_table = readGradientTable(volt_gradient_filename)
+            print(volt_gradient_table)
+            # schedule the gradient - nonblocking background scheduling
+            scheduleStepVoltage(serial_info, scheduler, volt_gradient_table, gradient_log_filename)
+            # print schedule
+            scheduler.print_jobs()
+        else: # if filename is empty
+            print("No Voltage scheme in the filename ...")
 
         # Wait until the acquisition is done
         while(q.GetRunManagerStatusFromCom == u"Acquiring"):
             sleep(5)
-            print("Acquisition ...")
+            print("Acquisition ...", volt_gradient_filename)
         sleep(1)
         # ensure power set to zero & set polarity to normal
         setPowerSupplyVoltage(serial_info, voltage = 0)
